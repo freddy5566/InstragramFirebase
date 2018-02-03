0987ad379b12e7c5de773a4516e949369589aa13
jamfly <cocoredju222@gmail.com>
2018-02-03 14:19:13 +0800

    lecture 1

---
+++ InstragramFirebase/ViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ViewController.swift
+//  InstragramFirebase
+//
+//  Created by freddy on 03/02/2018.
+//  Copyright © 2018 jamfly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - buttons and textfields
+    private let plusePhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(.alwaysOriginal), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let emailTextField: UITextField = {
+       let textField = UITextField()
+        textField.placeholder = "Email"
+        textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        textField.borderStyle = .roundedRect
+        textField.font = UIFont.systemFont(ofSize: 14)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private let usernameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Username"
+        textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        textField.borderStyle = .roundedRect
+        textField.font = UIFont.systemFont(ofSize: 14)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        textField.borderStyle = .roundedRect
+        textField.font = UIFont.systemFont(ofSize: 14)
+        textField.isSecureTextEntry = true
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.backgroundColor = UIColor(red: 149 / 255, green: 204 / 255, blue: 244 / 255, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        return button
+    }()
+    
+    private func setupInputFields() {
+        let stackView = UIStackView(
+            arrangedSubviews: [emailTextField,
+                               usernameTextField,
+                               passwordTextField,
+                               signUpButton
+            ])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: plusePhotoButton.bottomAnchor, constant: 20),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            stackView.heightAnchor.constraint(equalToConstant: 200)
+            ])
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        view.addSubview(plusePhotoButton)
+        
+        plusePhotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true
+        plusePhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        plusePhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        plusePhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+        
+        setupInputFields()
+    }
+
+    
+
+
+}
+
